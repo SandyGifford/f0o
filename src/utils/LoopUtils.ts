e73d11389782029b2df6bcde40551249ea5ebbef
Sanford Gifford <sanford.r.gifford@gmail.com>
2021-10-27 17:55:25 -0400

    remove unused

--- src/utils/LoopUtils.ts
+++
@@ -1,7 +0,0 @@
-export default class LoopUtils {
-	public static mapNTimes<T>(n: number, callback: (n: number) => T): T[] {
-		const arr: T[] = [];
-		for (let i = 0; i < n; i++) arr.push(callback(i));
-		return arr;
-	}
-}
